15e3782e8b47875ac0b562ca3f0e1debb4f1b2cd
weirdokat <soph.dulieu@gmail.com>
2017-08-29 10:43:13 +0200

    add Representative class

---
+++ src/model/Representative.java
@@ -0,0 +1,52 @@
+package model;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.collections.ObservableList;
+
+public class Representative extends Person {
+
+	private final DoubleProperty commissionRate;
+	private final DoubleProperty basicSalary;
+	
+	public Representative(Double commissionRate, Double basicSalary) {
+		
+		this.commissionRate = new SimpleDoubleProperty(commissionRate);
+		this.basicSalary = new SimpleDoubleProperty(basicSalary);
+	}
+	
+	public Representative() {
+		this(0d,0d);
+	}
+	
+	
+	public Double getCommissionRate() {
+		return commissionRate.get();
+	}
+	public void setCommissionRate(Double commissionRate) {
+		this.commissionRate.set(commissionRate);
+	}
+	public DoubleProperty commissionRateProperty() {
+		return commissionRate;
+	}
+	
+	
+	public Double getBasicSalary() {
+		return basicSalary.get();
+	}
+	public void setBasicSalary(Double basicSalary) {
+		this.basicSalary.set(basicSalary);
+	}
+	public DoubleProperty basicSalary() {
+		return basicSalary;
+	}
+	
+	public String toString() {
+		return ( getLastName() + " " + getFirstName() );
+	}
+	
+
+	public static ObservableList<Representant>getRepresentantList() {
+		return getRepresentantList();
+	}
+}
